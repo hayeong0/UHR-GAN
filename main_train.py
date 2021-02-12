fcc57ab3db96d2f444c935ea6b94cd02c0652474
chy0428 <21800758@handong.edu>
2021-02-12 20:50:47 +0900

    Modi main_train.py

--- main_train.py
+++ main_train.py
@@ -2,29 +2,55 @@ from config import get_arguments
 from SinGAN.manipulate import *
 from SinGAN.training import *
 import SinGAN.functions as functions
-
+import datetime
+import SinGAN.Augment as Augment
 
 if __name__ == '__main__':
     parser = get_arguments()
-    parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
+    parser.add_argument('--input_dir', help='input image dir', default='./MarbleData')
     parser.add_argument('--input_name', help='input image name', required=True)
     parser.add_argument('--mode', help='task to be done', default='train')
     opt = parser.parse_args()
-    opt = functions.post_config(opt)
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+    opt = functions.post_config(opt)    # 인자값 opt에 저장
     Gs = []
     Zs = []
     reals = []
     NoiseAmp = []
+    opt.input_name = opt.input_name
     dir2save = functions.generate_dir2save(opt)
 
+    opt.scaleList = [0.8,0.8,0.8,0.8,0.75,0.75,0.75,0.65,0.65,0.65,0.65,0.65] #fixed
+    opt.train_stages = len(opt.scaleList)
+    opt.continue_traing = 'N'
+    opt.cont = 'N'
     if (os.path.exists(dir2save)):
+        opt.cont = input("Trained model exist. Do you want to train the model again? N/Y ")
+
+    if(opt.cont == 'Y') :
+        starting_point = int(input("From what scale do you want to start training?"))
+        opt.starting_point = starting_point
+        opt.continue_traing = 'Y'
+
+    if (os.path.exists(dir2save) and (opt.cont == 'N')):
         print('trained model already exist')
+    
     else:
         try:
             os.makedirs(dir2save)
         except OSError:
             pass
-        real = functions.read_image(opt)
+        
+        # img_path = []
+        # img_path.append(opt.input_dir)
+        # img_path.append(opt.input_name)
+        # img_path = '/'.join(img_path)
+        #aug_real = Augment.Augment(img_path, opt)  
+        #functions.adjust_scales2image(aug_real, opt)
+
+        real = functions.read_image(opt)       
         functions.adjust_scales2image(real, opt)
         train(opt, Gs, Zs, reals, NoiseAmp)
         SinGAN_generate(Gs,Zs,reals,NoiseAmp,opt)
+
+       
--- random_samples.py
+++ random_samples.py
@@ -3,13 +3,14 @@ from SinGAN.manipulate import *
 from SinGAN.training import *
 from SinGAN.imresize import imresize
 import SinGAN.functions as functions
-
+import os
 
 if __name__ == '__main__':
     parser = get_arguments()
-    parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
+    parser.add_argument('--input_dir', help='input image dir', default='./MarbleData')
     parser.add_argument('--input_name', help='input image name', required=True)
     parser.add_argument('--mode', help='random_samples | random_samples_arbitrary_sizes', default='train', required=True)
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
     # for random_samples:
     parser.add_argument('--gen_start_scale', type=int, help='generation start scale', default=0)
     # for random_samples_arbitrary_sizes:
@@ -21,6 +22,8 @@ if __name__ == '__main__':
     Zs = []
     reals = []
     NoiseAmp = []
+    opt.scaleList = [0.8,0.8,0.8,0.8,0.75,0.75,0.75,0.65,0.65,0.65,0.65,0.65] #fixed
+    opt.train_stages = len(opt.scaleList)
     dir2save = functions.generate_dir2save(opt)
     if dir2save is None:
         print('task does not exist')
@@ -47,8 +50,3 @@ if __name__ == '__main__':
             Gs, Zs, reals, NoiseAmp = functions.load_trained_pyramid(opt)
             in_s = functions.generate_in2coarsest(reals,opt.scale_v,opt.scale_h,opt)
             SinGAN_generate(Gs, Zs, reals, NoiseAmp, opt, in_s, scale_v=opt.scale_v, scale_h=opt.scale_h)
-
-
-
-
-
