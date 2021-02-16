c87b36c79cc256d57cd4d7e5bedf19af66823df2
chy0428 <21800758@handong.edu>
2021-02-16 16:23:42 +0900

    Modi diff aug

--- SinGAN/DiffAugment.py
+++ SinGAN/DiffAugment.py
@@ -1,39 +1,26 @@
-# Differentiable Augmentation for Data-Efficient GAN Training
-# Shengyu Zhao, Zhijian Liu, Ji Lin, Jun-Yan Zhu, and Song Han
-# https://arxiv.org/pdf/2006.10738
-
+import cv2
 import torch
+import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from random import random
 from torchvision import transforms
-import cv2
-import matplotlib.pyplot as plt
 from torchvision.utils import save_image
 
-def rand_rotate(x, ratio=0.5):
+def rand_rotate(x, ratio=0.3):
     if ratio > random():
         return x
-
     else:  
         size = [int(i) for i in x.shape]
         h, w = size[2], size[3]
-        #print("TEST0: ", h, w)
+        # for debug
+        #print("TEST: ", x)
 
         aug = transforms.Compose([
             transforms.Pad(10, padding_mode = 'reflect'),
-            transforms.RandomRotation([0,5], expand=False),
+            transforms.RandomRotation(5, expand=False),
             transforms.CenterCrop([h,w])
         ])  
         x = aug(x)
-        # aug = transforms.Pad(10, padding_mode = 'reflect')
-        # x = aug(x)
-        # print("TEST2: ", x)
-        # aug = transforms.RandomRotation(10, expand=False)
-        # x = aug(x)
-        # print("TEST3: ", x)
-        # aug = transforms.CenterCrop([h,w])
-        # x = aug(x)
-        # print("SHAPE:", x.shape)
 
         # save image
         #img = x[0] 
@@ -47,12 +34,20 @@ def rand_affine(x, ratio=0.5):
     if ratio > random():
         return x
     else:  
-        aug = transforms.RandomAffine(degrees=10, translate=(0.2, 0.2),
-            scale=(0.8, 1.2), shear=15, resample=Image.BILINEAR)
+        size = [int(i) for i in x.shape]
+        h, w = size[2], size[3]
+
+        aug = transforms.Compose([
+            transforms.Pad(10, padding_mode = 'reflect'),
+            transforms.RandomAffine(degrees=10, translate=(0.2, 0.2),
+            scale=(0.8, 1.2), shear=15, resample=Image.BILINEAR),
+            transforms.CenterCrop([h,w])
+        ])  
         x = aug(x)
+
         return x
         
-def DiffAugment(x, policy='rotate', channels_first=True):
+def DiffAugment(x, policy='vflip,hflip', channels_first=True):
     if policy:
         if not channels_first:
             x = x.permute(0, 3, 1, 2)
@@ -64,14 +59,14 @@ def DiffAugment(x, policy='rotate', channels_first=True):
         x = x.contiguous()
     return x
 
-def rand_hflip(x, ratio=0.3):
+def rand_hflip(x, ratio=0.5):
     if ratio > random():
         return x
     else:
         x = torch.flip(x, dims=(3,))
         return x
 
-def rand_vflip(x, ratio=0.2):
+def rand_vflip(x, ratio=0.3):
     if ratio > random():
         return x
     else:
@@ -106,7 +101,7 @@ def rand_translation(x, ratio=0.125):
     )
     grid_x = torch.clamp(grid_x + translation_x + 1, 0, x.size(2) + 1)
     grid_y = torch.clamp(grid_y + translation_y + 1, 0, x.size(3) + 1)
-    x_pad = F.pad(x, [1, 1, 1, 1, 0, 0, 0, 0])
+    x_pad = F.pad(x, [1, 1, 1, 1, 1, 1, 1, 1])
     x = x_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2)
     return x
 
@@ -132,37 +127,8 @@ AUGMENT_FNS = {
     'color': [rand_brightness, rand_saturation, rand_contrast],
     'translation': [rand_translation],
     'cutout': [rand_cutout],
-    'vflip': [rand_vflip],
     'hflip': [rand_hflip],
+    'vflip': [rand_vflip],
     'rotate': [rand_rotate],
     'affine' : [rand_affine],
 }
-
-"""
-def rand_offset(x, ratio=1, ratio_h=1, ratio_v=1):
-    w, h = x.size(2), x.size(3)
-
-    imgs = []
-    for img in x.unbind(dim = 0):
-        max_h = int(w * ratio * ratio_h)
-        max_v = int(h * ratio * ratio_v)
-
-        value_h = random.randint(0, max_h) * 2 - max_h
-        value_v = random.randint(0, max_v) * 2 - max_v
-
-        if abs(value_h) > 0:
-            img = torch.roll(img, value_h, 2)
-
-        if abs(value_v) > 0:
-            img = torch.roll(img, value_v, 1)
-
-        imgs.append(img)
-
-    return torch.stack(imgs)
-
-def rand_offset_h(x, ratio=1):
-    return rand_offset(x, ratio=1, ratio_h=ratio, ratio_v=0)
-
-def rand_offset_v(x, ratio=1):
-    return rand_offset(x, ratio=1, ratio_h=0, ratio_v=ratio)
-"""
